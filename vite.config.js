74b6b9514c02f09fc84449b94c8fb7c4a5668638
cegs0612 <127986654+cegs0612@users.noreply.github.com>
2024-01-11 11:00:01 +0530

    debug deploy, try 3, removed .git from base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hangmangame.git",
+  base: "/hangmangame",
   plugins: [react()],
   server: {
     port: 3000,
